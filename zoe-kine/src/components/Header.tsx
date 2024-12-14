287eae686aa8826e19c63637ddfd017d8c437b5a
Jade <1822590@cegep-heritage.qc.ca>
2024-12-13 22:40:01 -0500

    Adding some pages and routing to the app

--- zoe-kine/src/components/Header.tsx
+++ zoe-kine/src/components/Header.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import '../css/Header.css'
+import React from 'react';
+import '../css/Header.css';
+import { Link } from 'react-router-dom';
 
-const Header:React.FC = () => {
-    return (
-        <header className="header">
-            <div className="logo">Zoweanne</div>
-            <nav>
-                <ul>
-                    <li><a href="#about">À propos de moi</a></li>
-                    <li><a href="#services">Mes services</a></li>
-                    <li><a href="#contact">Contactt</a></li>
-                    <li><a href="#book" className="button">Réserver en ligne</a></li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="logo">Zoweanne</div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">À propos de moi</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><a href="#book" className="button">Réserver en ligne</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- zoe-kine/src/main.tsx
+++ zoe-kine/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import {BrowserRouter} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
